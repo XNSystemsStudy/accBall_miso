a60fbb24f610b145530f9929188aed9e118b029c
misokim <muck_hyang@naver.com>
2016-07-11 13:01:43 +0900

    원이 화면에서 사라지지 않도록 수정

--- www/js/main.js
+++ www/js/main.js
@@ -5,14 +5,14 @@ myApp.controller("MainCtrl", ['$scope', '$timeout', function($scope, $timeout){
   $scope.watchID = null;
   $scope.innerWidth = window.innerWidth;
   $scope.innerHeight = window.innerHeight - 40;
-  $scope.posX = (innerWidth - 50) / 2;
-  $scope.posY = (innerHeight - 50) / 2;
-
   $scope.circle = {
     x : $scope.posX,
     y : $scope.posY,
     r : 40
   };
+  $scope.posX = ($scope.innerWidth - $scope.circle.r) / 2;
+  $scope.posY = ($scope.innerHeight - $scope.circle.r) / 2;
+
 
   $scope.startWatch = function() {
     console.log("startWatch");
@@ -44,6 +44,18 @@ myApp.controller("MainCtrl", ['$scope', '$timeout', function($scope, $timeout){
     
     $scope.posX += -1 * (acceleration.x * 1.5);
     $scope.posY += (acceleration.y * 1.5);
+
+    if($scope.posX < $scope.circle.r) {
+      $scope.posX = $scope.circle.r;
+    } else if($scope.posX > ($scope.innerWidth - $scope.circle.r)) {
+      $scope.posX = ($scope.innerWidth - $scope.circle.r);
+    }
+
+    if($scope.posY < $scope.circle.r) {
+      $scope.posY = $scope.circle.r;
+    } else if($scope.posY > ($scope.innerHeight - $scope.circle.r)) {
+      $scope.posY = ($scope.innerHeight - $scope.circle.r);
+    }
 
     $timeout(function() {  
       $scope.circle.x = $scope.posX;
